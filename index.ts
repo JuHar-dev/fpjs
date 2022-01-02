b67c262f1a6b025b9c53ed90fc4bacc5c251ac88
JuHar-dev <julian.hartl.dev@gmail.com>
2022-01-02 13:39:14 +0100

    Added index.ts

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { Either, left, right, Right, Left } from "./lib/either";
+export { Option, None, Some, none, some, optionOf } from "./lib/option";
+export { unit } from "./lib/unit";
+export { default as Failure } from "./lib/failure";
