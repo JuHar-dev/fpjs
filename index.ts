f8fb0dfa9098cae05db38c633159774e5502ae7b
JuHar-dev <julian.hartl.dev@gmail.com>
2022-01-02 13:46:07 +0100

    Added Unit

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export { Either, left, right, Right, Left } from "./lib/either";
 export { Option, None, Some, none, some, optionOf } from "./lib/option";
-export { unit } from "./lib/unit";
+export { unit, Unit } from "./lib/unit";
 export { default as Failure } from "./lib/failure";
